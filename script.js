f110974687a92809c5336eb11821f90dd3c527e3
Ryan <rdriscoll2000@gmail.com>
2023-11-23 19:25:04 -0500

    Finally finished this project

--- script.js
+++ script.js
@@ -1,97 +1,89 @@
+const options =['rock','scissors','paper'];
+
 function getComputerChoice(){
-  const choices = ["rock","paper","scissors"]
-  const randomIndex = Math.floor(Math.random() * choices.length);
-  const randomValue = choices[randomIndex];
-  return(randomValue);
+  const randomOptions = options[Math.floor(Math.random() * options.length)];
+  return randomOptions;
+}
+
+
+function DecideWinner(playerselection, computerselection){
+  if (playerselection == computerselection){
+    return "Tie";
+  }
+  else if(playerselection == 'rock'&& computerselection == 'scissors'||
+  playerselection == 'scissors' && computerselection == 'paper'||
+  playerselection == 'paper' && computerselection == 'rock'){
+    return 'Player';
+  }
+  else{
+    return 'Computer'
+  }
 
 
 }
-let playerselection =prompt("Please enter a move");
-let playerscore = 0;
-let computerscore = 0;
+function playRound(playerselection,computerselection){
+  const results = DecideWinner(playerselection,computerselection)
+  if (results == 'Tie'){
+    return 'You Tied with the computer!'
+  }
+  else if (results == 'Player'){
+    return 'Player won this round'
+  }
+  else{
+    return 'Computer won this round'
+  }
+}
 
-
-  function PlayRound(playerselection,computerselection){
-    
-
-    let RockPaperScissorsp = playerselection.toLowerCase();
-
-    if (RockPaperScissorsp===computerselection){
-      return "Tied Game";
+function PlayerChoice(){
+  let validatedinput = false;
+  while (validatedinput == false){
+    const choice = prompt('Rock Paper Scissors ');
+    if (choice == null){
+      continue;
     }
-      else if (RockPaperScissorsp === "paper" && computerselection === "rock"){
-        playerscore = ++playerscore;
-        return `Paper beats Rock. Player wins. Playerscore = ${playerscore}, Computerscore = ${computerscore}`;
-      }
-      else if (RockPaperScissorsp === "rock" && computerselection === "scissors"){
-        playerscore = ++playerscore;
-        return `Rock beats Scissors. Player wins. Playerscore = ${playerscore}, Computerscore = ${computerscore}`;
-      }
-      else if (RockPaperScissorsp === "scissors" && computerselection === "paper"){
-        playerscore = ++playerscore;
-        return `Scissors beats Paper. Player wins. Playerscore = ${playerscore}, Computerscore = ${computerscore}`;
-      }
-      else if (RockPaperScissorsp === "paper" && computerselection === "scissors"){
-        computerscore = ++computerscore;
-        return `Scissors beats Paper. Computer wins. Playerscore = ${playerscore}, Computerscore = ${computerscore}`;
-      }
-      else if (RockPaperScissorsp === "scissors" && computerselection === "rock"){
-        computerscore = ++computerscore;
-        return `Rock beats Scissors. Computer wins. Playerscore = ${playerscore}, Computerscore = ${computerscore}`;
-      }
-      else if (RockPaperScissorsp === "rock" && computerselection === "paper"){
-        computerscore = ++computerscore;
-        return `Paper beats Rock. Computer wins. Playerscore = ${playerscore}, Computerscore = ${computerscore}`;
-      } 
-      else{
-        return "That is not a valid response please enter Rock,Paper,or Scissors";
-      }
+    const choiceInLower = choice.toLowerCase();
+    if(options.includes(choiceInLower)){
+      validatedinput = true;
+      return choiceInLower;
+    }
   }
 
-PlayRound(playerselection,getComputerChoice());
-
-
+}
 
 
 function game(){
-
   let playerscore = 0;
   let computerscore = 0;
-  let playerselection =prompt("Please enter a move");
+  let results = DecideWinner
+ for (let i = 0; i<5; i++) {
+  playerselection = PlayerChoice(); 
+  computerselection = getComputerChoice();
+  console.log(playRound(playerselection,computerselection)); 
+  console.log('---------------------' )
 
-  for(let NumGames =0; NumGames<5;NumGames++){
-    if (playerscore>computerscore){
-      console.log(`Player Wins! Final Score: Player: ${playerscore}, Computer: ${computerscore}`)
-    }
-    else if (playerscore>computerscore){
-        console.log(`computer Wins! Final Score: Player: ${playerscore}, Computer: ${computerscore}`)
-    }
-    getComputerChoice();
-    let RockPaperScissorsp = playerselection.toLowerCase();
-    PlayRound(playerselection,getComputerChoice());
+  if (DecideWinner(playerselection,computerselection) == 'Tie'){
+  playerscore = playerscore;
+  computerscore = computerscore;
+  }
+
+  else if (DecideWinner(playerselection,computerselection) == 'Player'){
+    playerscore = ++playerscore;
+  }
+  else{
+    computerscore = ++computerscore;
+ }
 }
+console.log("Game Over");
+ 
+if(playerscore == computerscore){
+  console.log(`Game is tied no winner! Player score, ${playerscore} Computer score, ${computerscore}`)
 }
-game();
+else if (playerscore > computerscore){
+  console.log(`Congrats you have won! Player score, ${playerscore} Computer score, ${computerscore}`)
+ }
+ else{
+  console.log(`Damn you lost to a computer XD! Player score, ${playerscore} Computer score, ${computerscore}`)
+ }
 
-
-  
-
-
-//make varaible called choices to store the three values (Rock,Paper,Scissors)
-//make function call getComputerChoice (there is not going to be a parameter)
-//the code to be executed will take the variable with a random method
-//then make the code return the value
-//then invoke getComputerChoice
-
-
-//make varaibles called playerscore and computer
-//then make function called game that plays a 5 round game, keeps score, and reports who won.
-//
-
-
-//make a function called game
-//make variables to keep track of the player and computer scores
-//write a for loop the will play a round of RPS 5 times.
-//before each round make a prompt so the user can enter there answer
-//after each round display the results using console log
-//once the five games are finished display the winner and the final score
+}
+game()
