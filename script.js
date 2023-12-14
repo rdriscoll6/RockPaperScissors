10f29668b6c55a0d4d257f252230e560821f6625
Ryan <rdriscoll2000@gmail.com>
2023-12-13 22:33:50 -0500

    Finished project with working UI

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const options =['rock','scissors','paper'];
 
+
 function getComputerChoice(){
   const randomOptions = options[Math.floor(Math.random() * options.length)];
   return randomOptions;
@@ -18,72 +19,173 @@ function DecideWinner(playerselection, computerselection){
   else{
     return 'Computer'
   }
-
-
 }
+
 function playRound(playerselection,computerselection){
-  const results = DecideWinner(playerselection,computerselection)
+  const results = DecideWinner(playerselection,computerselection);
   if (results == 'Tie'){
-    return 'You Tied with the computer!'
+    console.log('You Tied with the computer!');
   }
   else if (results == 'Player'){
-    return 'Player won this round'
+    console.log('Player won this round');
   }
   else if (results == 'Computer'){
-    return 'Computer won this round'
+    console.log('Computer won this round');
   }
 }
 
-function PlayerChoice(){
-  let validatedinput = false;
-  while (validatedinput == false){
-    const choice = prompt('Rock Paper Scissors ');
-    if (choice == null){
-      continue;
-    }
-    const choiceInLower = choice.toLowerCase();
-    if(options.includes(choiceInLower)){
-      validatedinput = true;
-      return choiceInLower;
-    }
+const body = document.body;
+const rock = document.querySelector('#Rock');
+const scissors = document.querySelector('#Scissors');
+const paper = document.querySelector('#Paper');
+const userScore = document.querySelector('#userScore');
+const computerScore = document.querySelector('#computerScore');
+const userScoreNum = document.createElement('div');
+const computerScoreNum = document.createElement('div');
+const gameMessage = document.createElement('div');
+const resetButton = document.querySelector('#Reset')
+
+let i = 0;
+let x = 0;
+
+userScoreNum.textContent = i;
+computerScoreNum.textContent = x;
+userScore.appendChild(userScoreNum);
+computerScore.appendChild(computerScoreNum);
+gameMessage.textContent = 'First to 5 wins!'
+body.appendChild(gameMessage);
+
+rock.addEventListener('click', () => {
+  let playerselection = 'rock';
+
+  // Assuming DecideWinner returns the winner, adjust accordingly
+  let winner = DecideWinner(playerselection, getComputerChoice());
+
+  if (playerselection == getComputerChoice()) {
+    gameMessage.textContent = 'You Tied with the computer!';
+  } else if (
+    (playerselection == 'rock' && getComputerChoice() == 'scissors') ||
+    (playerselection == 'scissors' && getComputerChoice() == 'paper') ||
+    (playerselection == 'paper' && getComputerChoice() == 'rock')
+  ) {
+    gameMessage.textContent = 'Player won this round';
+    i = ++i;
+  } else {
+    gameMessage.textContent = 'Computer won this round';
+    x = ++x;
   }
 
+  // Update the scores after the winner is decided
+  userScoreNum.textContent = i;
+  computerScoreNum.textContent = x;
+  userScore.appendChild(userScoreNum);
+  computerScore.appendChild(computerScoreNum);
+  body.appendChild(gameMessage);
+
+  if (i ==5){
+  gameMessage.textContent = "Player won in 5 games";
+  i = 0;
+  x = 0;
 }
+else if (x ==5){
+  gameMessage.textContent = "Computer won in 5 games";
+  i = 0;
+  x = 0;
+}
+});
 
 
-function game(){
-  let playerscore = 0;
-  let computerscore = 0;
-  let results = DecideWinner
- for (let i = 0; i<5; i++) {
-  playerselection = PlayerChoice(); 
-  computerselection = getComputerChoice();
-  console.log(playRound(playerselection,computerselection)); 
-  console.log('---------------------' )
+paper.addEventListener('click',(e) => {
+  let playerselection = 'paper';
+  // Assuming DecideWinner returns the winner, adjust accordingly
+  let winner = DecideWinner(playerselection, getComputerChoice());
 
-  if (DecideWinner(playerselection,computerselection) == 'Tie'){
-  playerscore = playerscore;
-  computerscore = computerscore;
+  if (playerselection == getComputerChoice()) {
+    gameMessage.textContent = 'You Tied with the computer!';
+  } else if (
+    (playerselection == 'rock' && getComputerChoice() == 'scissors') ||
+    (playerselection == 'scissors' && getComputerChoice() == 'paper') ||
+    (playerselection == 'paper' && getComputerChoice() == 'rock')
+  ) {
+    gameMessage.textContent = 'Player won this round';
+    i = ++i;
+  } else {
+    gameMessage.textContent = 'Computer won this round';
+    x = ++x;
   }
 
-  else if (DecideWinner(playerselection,computerselection) == 'Player'){
-    playerscore = ++playerscore;
-  }
-  else{
-    computerscore = ++computerscore;
- }
-}
-console.log("Game Over");
- 
-if(playerscore == computerscore){
-  console.log(`Game is tied no winner! Player score, ${playerscore} Computer score, ${computerscore}`)
-}
-else if (playerscore > computerscore){
-  console.log(`Congrats you have won! Player score, ${playerscore} Computer score, ${computerscore}`)
- }
- else{
-  console.log(`Damn you lost to a computer XD! Player score, ${playerscore} Computer score, ${computerscore}`)
- }
+  // Update the scores after the winner is decided
+  userScoreNum.textContent = i;
+  computerScoreNum.textContent = x;
+  userScore.appendChild(userScoreNum);
+  computerScore.appendChild(computerScoreNum);
+  body.appendChild(gameMessage);
 
-}
-game()
+  if (i ==5){
+    gameMessage.textContent = "Player won in 5 games";
+    i = 0;
+    x = 0;
+  }
+  else if (x ==5){
+    gameMessage.textContent = "Computer won in 5 games";
+    i = 0;
+    x = 0;
+  }
+  
+});
+scissors.addEventListener('click',(e) => {
+  let playerselection = 'scissors';
+  // Assuming DecideWinner returns the winner, adjust accordingly
+  let winner = DecideWinner(playerselection, getComputerChoice());
+
+  if (playerselection == getComputerChoice()) {
+    gameMessage.textContent = 'You Tied with the computer!';
+  } else if (
+    (playerselection == 'rock' && getComputerChoice() == 'scissors') ||
+    (playerselection == 'scissors' && getComputerChoice() == 'paper') ||
+    (playerselection == 'paper' && getComputerChoice() == 'rock')
+  ) {
+    gameMessage.textContent = 'Player won this round';
+    i = ++i;
+  } else {
+    gameMessage.textContent = 'Computer won this round';
+    x = ++x;
+  }
+
+  // Update the scores after the winner is decided
+  userScoreNum.textContent = i;
+  computerScoreNum.textContent = x;
+  userScore.appendChild(userScoreNum);
+  computerScore.appendChild(computerScoreNum);
+  body.appendChild(gameMessage);
+
+  if (i ==5){
+    gameMessage.textContent = "Player won in 5 games &#127881";
+    i = 0;
+    x = 0;
+  }
+  else if (x ==5){
+    gameMessage.textContent = "Computer won in 5 games";
+    i = 0;
+    x = 0;
+  }
+});
+
+resetButton.addEventListener('click',()=>{
+  i = 0;
+  x = 0;
+  userScoreNum.textContent = i;
+  computerScoreNum.textContent = x;
+  userScore.appendChild(userScoreNum);
+  computerScore.appendChild(computerScoreNum);
+  gameMessage.textContent = 'First to 5 wins!';
+
+})
+gameMessage.style.color = 'white';
+gameMessage.style.fontSize = '50px';
+gameMessage.style.fontWeight = 'bold';
+gameMessage.style.textAlign = 'center';
+
+
+userScoreNum.style.color = 'white';
+computerScoreNum.style.color = 'white';
